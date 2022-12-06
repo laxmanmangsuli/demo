91a0cc3d136ff4d80e27827b986aaf833a638e77
Laxman IT <lbmangsuli79@gmail.com>
2022-12-06 21:58:40 +0530

    second commit

--- MS01_CMS-Eureka-Server/src/main/java/com/app/A.java
+++ MS01_CMS-Eureka-Server/src/main/java/com/app/A.java
@@ -2,7 +2,7 @@ package com.app;
 
 public class A {
 	public static void main(String[] args) {
-		System.out.println("This is git hub repository");
+		System.out.println("This is git hub repository first commit");
 	}
 
 }
