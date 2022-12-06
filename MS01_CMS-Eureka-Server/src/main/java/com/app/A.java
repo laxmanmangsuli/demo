a69ba0925a28ac065e0b2beff2f959898f386738
laxmanmangsuli <115659544+laxmanmangsuli@users.noreply.github.com>
2022-12-06 21:56:35 +0530

    change

--- MS01_CMS-Eureka-Server/src/main/java/com/app/A.java
+++ MS01_CMS-Eureka-Server/src/main/java/com/app/A.java
@@ -2,7 +2,7 @@ package com.app;
 
 public class A {
 	public static void main(String[] args) {
-		System.out.println("This is git hub");
+		System.out.println("This is git hub repository");
 	}
 
 }
